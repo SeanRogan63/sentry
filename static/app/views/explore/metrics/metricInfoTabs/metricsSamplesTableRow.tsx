961b945aa926ed568422dbd9c802a1a727716d85
nsdeschenes <nicholas.deschenes@sentry.io>
2026-05-07 10:58:44 -0300

    Only render on non-embedded tables

--- static/app/views/explore/metrics/metricInfoTabs/metricsSamplesTableRow.tsx
+++ static/app/views/explore/metrics/metricInfoTabs/metricsSamplesTableRow.tsx
@@ -253,7 +253,7 @@ export function SampleTableRow({
       </StickyTableRow>
       {isExpanded && (
         <ExpandedRowContainer>
-          <MetricDetails dataRow={row} ref={measureRef} />
+          <MetricDetails dataRow={row} ref={measureRef} showTelemetry={!embedded} />
         </ExpandedRowContainer>
       )}
     </TableRowContainer>
